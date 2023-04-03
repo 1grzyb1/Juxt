55d74d7b0e6acacb095043daa391c435cc621566
grzybek <grzybol.k@gmail.com>
2023-04-03 22:25:37 +0200

    feat: base of importing

--- src/engine/evaluator/evaluator_tests.rs
+++ src/engine/evaluator/evaluator_tests.rs
@@ -7,7 +7,8 @@ fn parse_content() {
         token_value: "Hello World".to_string(),
         content: None,
     }];
-    let result = eval(&nodes);
+    let js_code = generate_js("execute", "context", &nodes, &mock_provider);
+    let result = eval(&js_code);
     assert_eq!(result, "Hello World");
 }
 
@@ -30,7 +31,8 @@ fn parse_content_and_function() {
         token_value: "test()".to_string(),
         content: None,
     }];
-    let result = eval(&nodes);
+    let js_code = generate_js("execute", "context", &nodes, &mock_provider);
+    let result = eval(&js_code);
     assert_eq!(result, "Hello World=test");
 }
 
@@ -45,7 +47,8 @@ fn parse_each() {
             content: None,
         }]),
     }];
-    let result = eval(&nodes);
+    let js_code = generate_js("execute", "context", &nodes, &mock_provider);
+    let result = eval(&js_code);
     assert_eq!(result, "testtesttest");
 }
 
@@ -68,6 +71,34 @@ fn parse_each_with_function() {
             content: None,
         }]),
     }];
-    let result = eval(&nodes);
+    let js_code = generate_js("execute", "context", &nodes, &mock_provider);
+    let result = eval(&js_code);
     assert_eq!(result, "test1test2test3");
+}
+
+#[test]
+fn parse_import_component() {
+    let nodes = vec![Node {
+            token_type: TokenType::Import,
+            token_value: "component.juxt".to_string(),
+            content: None,
+        }, Node {
+        token_type: TokenType::Content,
+        token_value: "Hello World=".to_string(),
+        content: None,
+    }, Node {
+        token_type: TokenType::Function,
+        token_value: "component()".to_string(),
+        content: None,
+    }];
+    let js_code = generate_js("execute", "context", &nodes, &mock_provider);
+    let result = eval(&js_code);
+    assert_eq!(result, "Hello World=test");
+}
+
+fn mock_provider(template_name: &str, _: &str) -> String {
+    if template_name == "component.juxt" {
+        return "function component() { return 'test' }".to_string();
+    }
+    return "".to_string();
 }
--- src/engine/mod.rs
+++ src/engine/mod.rs
@@ -2,9 +2,20 @@ mod tokenizer;
 mod evaluator;
 mod tree_builder;
 
-pub fn execute(val: &str) -> String {
-    let tokens = tokenizer::tokenize(val);
-    let tree = tree_builder::build_tree(&tokens);
-    let result = evaluator::eval(&tree);
+struct Juxt {
+    name: String,
+    template: String,
+}
+
+pub fn execute(juxt: &str) -> String {
+    let js_code = compile(juxt, "execute");
+    let result = evaluator::eval(&js_code);
     return result;
+}
+
+fn compile(juxt: &str, fn_name: &str) -> String {
+    let tokens = tokenizer::tokenize(juxt);
+    let nodes = tree_builder::build_tree(&tokens);
+    let js_code = evaluator::generate_js(fn_name, "context", &nodes, &compile);
+    return js_code;
 }
