1c496b705e681f14910a46d24ae0b4f92100120a
grzybek <grzybol.k@gmail.com>
2023-04-09 00:27:08 +0200

    feat: execute and compile in lib

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,22 @@ use crate::engine::{compile, execute, Juxt};
 
 pub mod engine;
 
-
-pub fn compile_and_execute(main: Juxt, dependencie: Vec<Juxt>, context: String) -> Result<String, Box<dyn std::error::Error>> {
-    let compiled = compile(main, dependencie)?;
+pub fn compile_and_execute(
+    main: Juxt,
+    dependencies: Vec<Juxt>,
+    context: String,
+) -> Result<String, Box<dyn std::error::Error>> {
+    let compiled = compile(main, dependencies)?;
     return execute(&compiled, context);
+}
+
+pub fn execute_js(js_code: &str, context: String) -> Result<String, Box<dyn std::error::Error>> {
+    return execute(js_code, context);
+}
+
+pub fn execute_flux(
+    main: Juxt,
+    dependencies: Vec<Juxt>,
+) -> Result<String, Box<dyn std::error::Error>> {
+    return compile(main, dependencies);
 }
